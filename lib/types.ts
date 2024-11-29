2c26872769cd4b406666446803098dd6ac456773
ariframaps <ariframa@student.ub.ac.id>
2024-11-30 00:36:14 +0700

    fix type

--- lib/types.ts
+++ lib/types.ts
@@ -1,9 +1,22 @@
 export type ProductCardType = {
-  id: number;
+  id: string;
   name: string;
   image: string;
   price: number;
   sellerId: string;
+};
+
+export type ProductType = {
+  id: string;
+  name: string;
+  image: string;
+  description: string;
+  price: number;
+  difficulty: "Beginner" | "Intermediate" | "Advanced";
+  category: string;
+  link: string;
+  sellerId: string;
+  isDeleted: false;
 };
 
 export type NavLinkType = {
