6b611fb21797d93a4cbce5ca0a608672ab0871f4
ariframaps <ariframa@student.ub.ac.id>
2024-12-04 22:02:09 +0700

    add payment development warning

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -3,6 +3,8 @@
 import CartCard from "@/components/CartCard";
 import PaymentForm from "@/components/PaymentForm";
 import { useCart } from "@/provider/context/CartContext";
+import { Alert } from "flowbite-react";
+import { AlertTriangleIcon } from "lucide-react";
 
 const Page = () => {
   const { cart, totalPrice } = useCart();
@@ -27,6 +29,34 @@ const Page = () => {
               registered email{" "}
             </p>
           </div>
+          <Alert className="bg-red-300 mx-3">
+            <div className="w-64 sm:w-4/5">
+              <span className="font-medium">Peringatan!</span> Jangan membayar
+              dengan payment asli, karena ini adalah development mode. silahkan
+              menggunakan payment simulator dari midtrans di instruksi dibawah
+              ini (contoh menggunakan QRIS).
+              <ul className="ms-5">
+                <li className="list-disc">
+                  Isi form dibawah dan klik checkout
+                </li>
+                <li className="list-disc">
+                  Pilih metode pembayaran (Gopay/Gopay later)
+                </li>
+                <li className="list-disc">
+                  Copy image address dari barcode QRIS
+                </li>
+                <li className="list-disc">
+                  masukkan link image di sini (klik):{" "}
+                  <a
+                    className="text-blue-700"
+                    target="_blank"
+                    href="https://simulator.sandbox.midtrans.com/v2/qris/index">
+                    Payment Simulator
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </Alert>
           <PaymentForm />
         </div>
       </div>
