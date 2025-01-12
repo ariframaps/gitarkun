f2ebc3e06665864daa4b559a4adfdecf6e9005f1
ariframaps <ariframa@student.ub.ac.id>
2025-01-13 03:23:29 +0700

    fix filter button

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -73,7 +73,7 @@ const Page = () => {
                 />
                 <button
                   type="submit"
-                  className=" absolute end-2.5 bottom-2.5 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800  bg-yellow-400 border border-black hover:bg-yellow-500 text-black">
+                  className="rounded-md border-none absolute end-2.5 bottom-2.5 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800  bg-yellow-400 border border-black hover:bg-yellow-500 text-black">
                   Search
                 </button>
               </div>
--- components/FilterSideBar.tsx
+++ components/FilterSideBar.tsx
@@ -97,7 +97,7 @@ const FilterSideBar = () => {
               </div>
               <button
                 onClick={() => clearDispatch()}
-                className=" bg-yellow-400 text-black hover:bg-yellow-600 text-center flex items-center focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2 md:py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                className=" bg-yellow-400 w-max px-4 text-black hover:bg-yellow-600 text-center flex items-center focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2 md:py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Clear
               </button>
             </li>
